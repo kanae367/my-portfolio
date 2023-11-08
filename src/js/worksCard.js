5034a53ab5f36c8d412b3a294f8415b7a372f9a9
”Kodoku” <kodokunawa@gmail.com>
2023-11-08 13:15:13 +0300

    fix: incorrect hover behavior

--- src/js/worksCard.js
+++ src/js/worksCard.js
@@ -16,19 +16,19 @@ const cadrButtonClickHandler = (e) => {
     }
 }
 
-const cards = document.querySelectorAll('.card__content');
+const cards = document.querySelectorAll('.card__technologies');
 
 const cardMouseOutHandler = function() {
-    this.classList.remove('card__content_active');
+    this.closest(".card__content").classList.remove('card__content_active');
     this.removeEventListener("mouseout", cardMouseOutHandler);
 }
 
-const cardMouseOverHandler = function(){
-    this.classList.add('card__content_active');
+const cardMouseOverHandler = function() {
+    this.closest(".card__content").classList.add('card__content_active');
     this.addEventListener('mouseout', cardMouseOutHandler);
 }
 
 cards.forEach(card => {
     card.addEventListener('mouseover', cardMouseOverHandler);
-    card.querySelector('button').addEventListener('click', cadrButtonClickHandler);
+    card.closest(".card__content").querySelector('button').addEventListener('click', cadrButtonClickHandler);
 })
