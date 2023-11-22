075745a155fd90cbf0335fd89544141aa7474cf4
”Kodoku” <kodokunawa@gmail.com>
2023-11-22 22:14:32 +0300

    feat: new mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -13,4 +13,35 @@
       socials.classList.remove('scrolled');
     }
   })
+
+  const openNavButton = document.getElementById('nav-button');
+  const navMenu = document.querySelector('.menu_nav');
+
+  openNavButton.addEventListener('click', () => {
+    navMenu.classList.add('active');
+  });
+
+  const openSocialsButton = document.getElementById('socials-button');
+  const socialsMenu = document.querySelector('.menu_socials');
+
+  openSocialsButton.addEventListener('click', () => {
+    socialsMenu.classList.add('active');
+  });
+
+  const closeButtons = document.querySelectorAll('.close-button');
+  closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      button.closest('.menu').classList.remove('active');
+    })
+  });
+
+  const mobileNav = document.querySelector('.mobile-nav');
+  document.addEventListener('scroll', () => {
+    if(window.scrollY > 600){
+      mobileNav.classList.add('mobile-nav_active');
+    }else{
+      mobileNav.classList.remove('mobile-nav_active');
+    }
+  })
+
 })()
